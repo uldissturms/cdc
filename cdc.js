328264b4092046071307226e378e3f2a462edd36
Uldis Sturms <uldis.sturms@gmail.com>
2016-12-05 01:46:11 +0000

    provide mock server for consumer contracts

---
+++ cdc.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+const program = require('commander')
+const path = require('path')
+const pkg = require('./package')
+const mock = require('./lib/mock')
+
+program
+  .version(pkg.version)
+  .command('mock [contract]')
+  .option('-p, --port <port>', 'Port for running mock server. Defaults to 3000.')
+  .description('Provide mock responses for matching consumer requests.')
+  .action((contract = './contracts', options) => {
+    mock(path.resolve(contract), options.port || 3000)
+  })
+
+program.parse(process.argv)
+
+if (program.args.length === 0) {
+  program.help()
+}
