eb95604ae60295d95b863493d82a5bb226fa155a
Uldis Sturms <uldis.sturms@gmail.com>
2016-12-18 11:15:23 +0000

    expose contract verification

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const program = require('commander')
 const path = require('path')
 const pkg = require('./package')
 const mock = require('./mock')
+const verify = require('./verify')
 const fs = require('fs')
 
 const server = (contract, options) =>
@@ -30,12 +31,22 @@ const mockServer = (contract, options) => {
 
 program
   .version(pkg.version)
+
+program
   .command('mock [contract]')
   .option('-p, --port <port>', 'Port for running mock server. Defaults to 3000.')
   .option('-w, --watch', 'Watch for changes.')
   .description('Provide mock responses for matching consumer requests.')
   .action((contract = './contracts', options) => {
     mockServer(contract, options)
+  })
+
+program
+  .command('verify [contract]')
+  .option('-b, --baseUrl <port>', 'Base url to run verifications against.')
+  .description('Verify contracts against provider.')
+  .action((contract = './contracts', options) => {
+    verify(contract, options.baseUrl)
   })
 
 program.parse(process.argv)
